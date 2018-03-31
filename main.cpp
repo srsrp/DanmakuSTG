5a10026847ebd53fd76f1bbd5e9fb90fe49f93e0
Reverse065 <36537982+srsrp@users.noreply.github.com>
2018-03-31 16:01:21 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,41 +6,41 @@
 #include "Enemy.h"
 #include "Barrage.h"
 
-//ˆê”Ê‚Ì•Ï”
+//ä¸€èˆ¬ã®å¤‰æ•°
 int LoopCount = 0;
 int n, m;
 char KeyBuffer[256];
 
-//˜@q‚Ì‰ŠúˆÊ’u
+//è“®å­ã®åˆæœŸä½ç½®
 int RenkoX = (LEFT + RIGHT) / 2;
 int RenkoY = (UP + DOWN) / 2 + 200;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	//‰æ‘œHandle
+	//ç”»åƒHandle
 	char renkohd[50] = "renko.png";								//30*40
 	char renkobarrage001hd[50] = "renkobarrage001.png";			//30*15
 	char enemy001hd[50] = "enemy001.png";						//30*40
 	char enemybarrage001hd[50] = "enemybarrage001.png";			//16*16
 
-	//”wŒi‚ÉŠÖ‚·‚é•Ï”
+	//èƒŒæ™¯ã«é–¢ã™ã‚‹å¤‰æ•°
 	unsigned int fuji;
 
-	if (DxLib_Init() == -1) { return -1; }		//DXlib‚ÌŒÄ‚Ño‚µ
+	if (DxLib_Init() == -1) { return -1; }		//DXlibã®å‘¼ã³å‡ºã—
 
 	ChangeWindowMode(TRUE);
 	SetWindowSize(960, 720);
 	SetWindowPosition(0, 10);
 
-	//©‹@
+	//è‡ªæ©Ÿ
 	Player Renko(RenkoX, RenkoY, 30, 40, TRUE, renkohd, 5);
 
-	Barrage RenkoBarrage[100];
+	Barrage RenkoBarrge001[100];
 	for (n = 0; n < 100; n++) {
-		RenkoBarrage[n] = Barrage(0, 0, 30, 15, FALSE, renkobarrage001hd, 12, 0);
+		RenkoBarrge001[n] = Barrage(0, 0, 30, 15, FALSE, renkobarrage001hd, 12, 0);
 	}
 
-	//100ƒ‹[ƒv‚Åo‚Ä‚­‚é“G
+	//100ãƒ«ãƒ¼ãƒ—ã§å‡ºã¦ãã‚‹æ•µ
 	Enemy Enemy001(420, 100, 30, 40, FALSE, enemy001hd, 5);
 
 	Barrage Enemy001Barrage[100];
@@ -48,7 +48,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		Enemy001Barrage[n] = Barrage(0, 0, 16, 16, FALSE, enemybarrage001hd, 12, 0);
 	}
 
-	//”wŒi
+	//èƒŒæ™¯
 	fuji = GetColor(187, 188, 222);
 
 	SetDrawScreen(DX_SCREEN_BACK);
@@ -61,49 +61,49 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 		GetHitKeyStateAll(KeyBuffer);
 
-		//˜@qü‚è‚Ì•\¦
-		//˜@q‚ÌˆÚ“®
+		//è“®å­å‘¨ã‚Šã®è¡¨ç¤º
+		//è“®å­ã®ç§»å‹•
 
 		if (Renko.ScreenFlag == FALSE)Renko.Display();
 		Renko.Move();
 
-		//˜@q‚Ì’e–‹‚ÌˆÚ“®
+		//è“®å­ã®å¼¾å¹•ã®ç§»å‹•
 		for (n = 0; n < 30; n++) {
-			if (KeyBuffer[KEY_INPUT_Z] && RenkoBarrage[n].ScreenFlag == FALSE) {
-				RenkoBarrage[n].Gx = Renko.Gx;				//’†S‚Æ‚¸‚ê‚é
-				RenkoBarrage[n].Gy = Renko.Gy;				//“¯ã
-				RenkoBarrage[n].ScreenFlag = TRUE;
+			if (KeyBuffer[KEY_INPUT_Z] && RenkoBarrge001[n].ScreenFlag == FALSE) {
+				RenkoBarrge001[n].Gx = Renko.Gx;				//ä¸­å¿ƒã¨ãšã‚Œã‚‹
+				RenkoBarrge001[n].Gy = Renko.Gy;				//åŒä¸Š
+				RenkoBarrge001[n].ScreenFlag = TRUE;
 				break;
 			}
 		}
 
 		for (n = 0; n < 30; n++) {
-			if (RenkoBarrage[n].ScreenFlag == TRUE) {
-				RenkoBarrage[n].Display();
-				RenkoBarrage[n].Move(0, RenkoBarrage[n].RunSpeed);
-				if (RenkoBarrage[n].Gy < UP)RenkoBarrage[n].ScreenFlag = FALSE;
+			if (RenkoBarrge001[n].ScreenFlag == TRUE) {
+				RenkoBarrge001[n].Display();
+				RenkoBarrge001[n].Move(0, RenkoBarrge001[n].RunSpeed);
+				if (RenkoBarrge001[n].Gy < UP)RenkoBarrge001[n].ScreenFlag = FALSE;
 			}
 		}
 
-		//“Gü‚è‚Ì•\¦
-		//LoopCount=101‚ÅÅ‰‚Ì“G“oê
+		//æ•µå‘¨ã‚Šã®è¡¨ç¤º
+		//LoopCount=101ã§æœ€åˆã®æ•µç™»å ´
 
 		if (LoopCount > 100)Enemy001.ScreenFlag = TRUE;
 
 		if (Enemy001.ScreenFlag == TRUE) {
 			Enemy001.Display();
 
-			//CENTER‚Ü‚Å‰E‚©‚çˆÚ“®‚µ‚Ä‚­‚é
+			//CENTERã¾ã§å³ã‹ã‚‰ç§»å‹•ã—ã¦ãã‚‹
 			if (Enemy001.Gx > CENTER)Enemy001.Move(3, 3);
-			//’e–‹‚ğŒ‚‚Á‚Ä‚¢‚éŠÔÃ~
+			//å¼¾å¹•ã‚’æ’ƒã£ã¦ã„ã‚‹é–“é™æ­¢
 			if (Enemy001Barrage[24].ScreenFlag == TRUE) {
 				Enemy001.Move(3, 3);
 			}
 			/*
-			//“–‚½‚è”»’è
+			//å½“ãŸã‚Šåˆ¤å®š
 			for (n = 0; n < 100; n++) {
-				RenkoBarrage[n].Distance = sqrt((RenkoBarrage[n].x - Enemy001.x)*(RenkoBarrage[n].x - Enemy001.x) + (RenkoBarrage[n].y - Enemy001.y)*(RenkoBarrage[n].y - Enemy001.y));
-				if (RenkoBarrage[n].Distance <= 10) {
+				RenkoBarrge001[n].Distance = sqrt((RenkoBarrge001[n].x - Enemy001.x)*(RenkoBarrge001[n].x - Enemy001.x) + (RenkoBarrge001[n].y - Enemy001.y)*(RenkoBarrge001[n].y - Enemy001.y));
+				if (RenkoBarrge001[n].Distance <= 10) {
 					Enemy001.ScreenFlag = FALSE;
 				}
 
@@ -111,7 +111,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			*/
 		}
 
-		//CENTER‚Ü‚Å—ˆ‚½‚ç’e–‹‚ğŒ‚‚Â
+		//CENTERã¾ã§æ¥ãŸã‚‰å¼¾å¹•ã‚’æ’ƒã¤
 		if (Enemy001.Gx <= CENTER) {
 			for (n = 0; n < 100; n++) {
 				if (Enemy001Barrage[n].ScreenFlag == FALSE && LoopCount % 2 == 0) {
@@ -132,7 +132,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			}
 		}
 
-		//˜@q‚Ì“–‚½‚è”»’è
+		//è“®å­ã®å½“ãŸã‚Šåˆ¤å®š
 		/*
 		for (n = 0; n < 100; n++) {
 			Enemy001Barrage[n].Distance = sqrt((Enemy001Barrage[n].x - Renko.x)*(Enemy001Barrage[n].x - Renko.x) + (Enemy001Barrage[n].y - Renko.y)*(Enemy001Barrage[n].y - Renko.y));
