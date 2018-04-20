30df7724632850533f2f0150765d0c1aae443fd6
Reverse065 <36537982+srsrp@users.noreply.github.com>
2018-04-20 21:46:33 +0900

    180420UpDate

--- Barrge.cpp
+++ Barrge.cpp
@@ -1,33 +1,46 @@
 #include "DxLib.h"
+#include "Constant.h"
 #include "Barrage.h"
+#include <cmath>
 
-Barrage::Barrage(){}
+Barrage::Barrage() {}
 
-Barrage::Barrage(int Gx_, int Gy_, int GraphicSizeX_, int GraphicSizeY_, bool ScreenFlag_, char* FileName_, int RunSpeed_, int Distance_)
-	:Object(Gx_, Gy_, GraphicSizeX_, GraphicSizeY_, ScreenFlag_, FileName_), RunSpeed(RunSpeed_), Distance(Distance_)
+Barrage::Barrage(double Gx_, double Gy_, double Speed_, int GraphSizeX_, int GraphSizeY_, bool ScreenFlag_, char* FileName_)
+	:Object(Gx_, Gy_, Speed_, GraphSizeX_, GraphSizeY_, ScreenFlag_, FileName_)
 {}
 
-void Barrage::Move(int Way_, int RunSpeed_) {
-	switch (Way_) {
-	case 0://��
-		Gy -= RunSpeed_;
-		y -= Gy + GraphicSizeY / 2;
-		break;
-	case 1://�E
-		Gx += RunSpeed_;
-		x += Gx + GraphicSizeX / 2;
-		break;
-	case 2://��
-		Gy += RunSpeed_;
-		y += Gy + GraphicSizeY / 2;
-		break;
-	case 3://��
-		Gx -= RunSpeed_;
-		x -= Gx + GraphicSizeX / 2;
-		break;
-	}
+void Barrage::SetUp(int LoopCount_, int MovePattern_, double Speed_, double Gx_, double Gy_, double A_, double B_, double C_) {
+	ScreenFlag = TRUE;
+	LoopCountIni = LoopCount_;
+	MovePattern = MovePattern_;
+	Speed = Speed_;
+	Gx = Gx_;
+	Gy = Gy_;
+	A = A_;
+	B = B_;
+	C = C_;
 }
 
-void Barrage::Hit(Object Object_) {
-	//Distance=
+void Barrage::Move() {
+	if (ScreenFlag == TRUE)Display();
+	double Time = LoopCount - LoopCountIni;
+
+	switch (MovePattern) {
+
+	case 0://ｙ座標を減らすだけ
+		Gy += Speed;
+		break;
+
+	case 1://ななめに撃つ
+		Gx += Speed * cos(PI / 180 * A);
+		Gy += Speed * sin(PI / 180 * A);
+		break;
+
+	case 2:
+		break;
+
+	case 3:
+		break;	
+	}
+	if (Gx<LEFT - 50 || Gx>RIGHT + 50 || Gy<UP - 50 || Gy>DOWN + 50)ScreenFlag = FALSE;
 }
