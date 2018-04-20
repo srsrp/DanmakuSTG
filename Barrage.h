df2fd8395ee5e8712d30a8cb86984e5215aff1d2
Reverse065 <36537982+srsrp@users.noreply.github.com>
2018-04-20 21:45:50 +0900

    180420UpDate

--- Barrage.h
+++ Barrage.h
@@ -4,15 +4,22 @@
 #include "Object.h"
 
 class Barrage :public Object {
+private:
+
+	int LoopCountIni;
+	int MovePattern;	//移動パターン
+	double A, B, C;		//パラメータ。必要に応じて個数追加すべし。
+
+	double Distance;
+	
 public:
-	int RunSpeed;
-	int Distance;
-
+	
 	Barrage();
-	Barrage(int Gx_, int Gy_, int GraphicSizeX_, int GraphicSizeY_, bool ScreenFlag_, char* FileName_, int RunSpeed_, int Distance_);
+	Barrage(double Gx_, double Gy_, double Speed_, int GraphSizeX_, int GraphSizeY_, bool ScreenFlag_, char* FileName_);
+	
+	void SetUp(int LoopCount_, int MovePattern_, double Speed_, double Gx_, double Gy_, double A_, double B_, double C_);
+	void Move();
 
-	void Move(int Way_, int runSpeed);
-	void Hit(Object Object_);
 };
 
 #endif
