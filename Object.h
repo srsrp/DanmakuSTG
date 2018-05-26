6dd8fb8b6096c89bb14e7c2dd47f5a47812a69fd
Reverse065 <36537982+srsrp@users.noreply.github.com>
2018-05-26 09:38:22 +0900

    0526UpData
    使用しなくなった。

--- Object.h
+++ Object.h
@@ -1,23 +1,26 @@
-#ifndef INCLUDED_OBJECT_H_
-#define INCLUDED_OBJECT_H_
+#include "DxLib.h"
+#include "Object.h"
+#include "Constant.h"
 
-class Object {
-private:
-	int GraphicSizeX, GraphicSizeY;
+Object::Object(){}
 
-protected:
-	int Handle;
-	int GraHandle[2];
-	double Speed;
+Object::Object(double Gx_, double Gy_,double Speed_, int GraphicSizeX_, int GraphicSizeY_, bool ScreenFlag_, char* FileName_) :
+	Gx(Gx_), Gy(Gy_), Speed(Speed_), GraphicSizeX(GraphicSizeX_), GraphicSizeY(GraphicSizeY_), ScreenFlag(FALSE)
+{
+	//Handle = LoadGraph(FileName_);
+	Handle = LoadDivGraph(FileName_, 3, 3, 1, 60, 80, GraHandle);
+}
 
-public:
-	double Gx, Gy;		//ćĚŔW
-	bool ScreenFlag;
-	Object();
-	Object(double Gx_, double Gy_, double Speed_, int GraphicSizeX_, int GraphicSizeY_, bool ScreenFlag_, char* FileName_);
-	
-	void Display();
+void Object::Display() {
+	//DrawGraph(Gx, Gy, Handle, TRUE);
+	if (LoopCount % 21 >= 0 && LoopCount % 21 <= 6) {
+		DrawExtendGraph(Gx, Gy, Gx + GraphicSizeX + 1, Gy + GraphicSizeY + 1, GraHandle[0], TRUE);
+	}
+	if (LoopCount % 21 >= 7 && LoopCount % 21 <= 13) {
+		DrawExtendGraph(Gx, Gy, Gx + GraphicSizeX + 1, Gy + GraphicSizeY + 1, GraHandle[1], TRUE);
+	}
+	if (LoopCount % 21 >= 14 && LoopCount % 21 <= 20) {
+		DrawExtendGraph(Gx, Gy, Gx + GraphicSizeX + 1, Gy + GraphicSizeY + 1, GraHandle[2], TRUE);
+	}
 
-};
-
-#endif
+}
