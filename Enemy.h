0f102eb0b553595cc93261f4ea9b8d693fcd1677
Reverse065 <36537982+srsrp@users.noreply.github.com>
2018-05-26 09:36:41 +0900

    0526UpData
    敵弾幕の要素もここに書き加えた。BOOL1000(=bool[20][50])などはConstant.hに記述。

--- Enemy.h
+++ Enemy.h
@@ -1,25 +1,75 @@
 #ifndef INCLUDED_ENEMY_H_
 #define INCLUDED_ENEMY_H_
 
-#include "Object.h" 
+#include "Player.h"
+#include "Constant.h"
 
-class Enemy :public Object {
+class Enemy{
+	/*
+	オブジェクト宣言時に敵の形、弾幕の形を与える。
+	Setで両方のデータを設定。
+	*/
 private:
+	/*共通*/
+	INT20			LoopCountIni;			//出現ループ数
+	void Set(int LoopCount_, EnePattern EnemyPatter_, double Speed_, double Gx_, double Gy_, BarPattern BarragePattern_, double BarrageSpeed_, double A_ = 0, double B_ = 0, double C_ = 0, double a_ = 0, double b_ = 0, double c_ = 0, double d_ = 0, double e_ = 0);
+	
+	/*敵*/	
+	enum EneDesign	EnemyDesign;			//デザイン
+	enum EnePattern	EnemyPattern[ENE_MAX];	//移動パターン
 
-	int MovePattern;			//敵の移動パターン
-	int LoopCountIni;			//出現ループ数
-	double A, B, C;				//移動に関するパラメータ
+	DBL20			Speed;					//速さ
+	
+	DBL20			A,
+					B,
+					C;						//パラメータ
 
+	int				Handle;					//画像ハンドル
+	int				GrSizeX,
+					GrSizeY;				//画像サイズ
+	
+	
+	void			EnemyDisplay();			//表示関数
+	void			EnemyMove();
+
+	/*弾幕*/
+	enum BarDesign	BarrageDesign;			//デザイン
+	enum BarPattern	BarragePattern[20];		//移動パターン
+
+	BOOL20			BarrageUseFlag;			//弾幕配列を使っているかのフラグ
+
+	BOOL1000		BarrageScreenFlag;		//画面上にいるかのフラグ
+	DBL1000			BarrageSpeed;			//速さ
+	DBL1000			BarrageGx;
+	DBL1000			BarrageGy;				//座標(左上)
+	DBL1000			BarrageDistance;		//当たり判定用
+	DBL20			a,
+					b,
+					c,
+					e,
+					d;						//パラメータ
+
+	int				BarrageHandle;			//画像ハンドル
+	int				BarrageGrSizeX,
+					BarrageGrSizeY;			//画像サイズ
+
+
+	void			BarrageMove(Player Player_);
+	void			BarrageDisplay();		//表示関数
 public:
-
+	DBL20			Gx,
+					Gy;						//座標
+	BOOL20			ScreenFlag;				//画面上にいるかのフラグ
+	double			EnemyRadius = 15.0;		//敵の当たり判定半径
+	double			BarrageRadius = 5.0;	//弾幕の当たり判定半径
 	//コンストラクタ
 	Enemy();
-	Enemy(double Gx_, double Gy_, double Speed_, int GraphSizeX_, int GraphSizeY_, bool ScreenFlag_, char* FileName_);
+	Enemy(char* FileName_, EneDesign EnemyDesign_, int SizeX_, int SizeY_, char* BarFileName_, BarDesign BarrageDesign_, int BarSizeX_, int BarSizeY_);
 
 	//移動に関するメソッド
-	void SetUp(int LoopCount_, int MovePattern_, double Speed_, double x_, double y_, double A_, double B_, double C_);
-	void Move();									//ループごとに呼び出し。SetUp関数に従って移動。
-
+	void SetUp();
+	void Move(Player Player_);
+	void KillCheck(Player *Player_);
 };
 
 #endif
