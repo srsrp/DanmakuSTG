869fc3fefb7f819fa1cbce9757b8a13c8e7efb66
Reverse065 <36537982+srsrp@users.noreply.github.com>
2018-05-26 09:27:39 +0900

    0526UpData
    Barrage　classは使わなくなったので全体をコメントアウト

--- Barrage.h
+++ Barrage.h
@@ -1,25 +1,49 @@
-#ifndef INCLUDED_BARRAGE_H_
+/*#ifndef INCLUDED_BARRAGE_H_
 #define INCLUDED_BARRAGE_H_
 
-#include "Object.h"
+#include "Player.h"
+#include "Enemy.h"
+#include "Constant.h"
 
-class Barrage :public Object {
+class Barrage{
 private:
 
-	int LoopCountIni;
-	int MovePattern;	//移動パターン
-	double A, B, C;		//パラメータ。必要に応じて個数追加すべし。
-
-	double Distance;
+	BOOL20			EneFlag;				//対応する敵のフラグ
 	
+	BOOL20			EneX,
+					EneY;					//敵の座標
+	enum BarType	Type;					//弾幕のデザイン：SetUp()で使う。列挙型
+	enum BarPat		BarPattern[20];			//敵の移動パターン。列挙型でパターンわかり易く。
+
+	INT20			LoopCountIni;			//出現ループ数
+	BOOL1000		BarScreenFlag;			//画面上にいるかどうかのフラグ
+	DBL1000			Speed;					//移動する速さ
+	DBL1000			Gx;
+	DBL1000			Gy;						//座標(左上)
+	DBL20			a,
+					b,
+					c,
+					e,
+					d;						//弾幕に関するパラメータ
+		
+	int				Handle;					//画像ハンドル
+	int				GrSizeX,
+					GrSizeY;				//画像サイズ	
 public:
+	BOOL20			BarFlag;				//使ってるかどうかのフラグ
+	
+	//あいであ①
+	//初期座標をセッティングして、敵と同じパターンで動かして（SF＝FALSEのまま）、
+	//必要になればSF=TRUE
+	//〇敵の座標を渡す必要なし。
 	
 	Barrage();
-	Barrage(double Gx_, double Gy_, double Speed_, int GraphSizeX_, int GraphSizeY_, bool ScreenFlag_, char* FileName_);
+	Barrage(Enemy Enemy_, char* FileName_, BarType Type_, int GrSizeX_, int GrSizeY_);
 	
-	void SetUp(int LoopCount_, int MovePattern_, double Speed_, double Gx_, double Gy_, double A_, double B_, double C_);
-	void Move();
-
+	void BarSetUp(Enemy Enemy_);
+	void BarMove(Enemy Enemy_,Player Player_);
+	void BarSet(int LoopCount_, BarPat BarPattern_, double Speed_,Enemy Enemy_, double a_ = 0, double b_ = 0, double c_ = 0, double d_ = 0, double e_ = 0);
+	void BarDisplay();
 };
 
-#endif
+#endif*/
